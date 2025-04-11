eac10e0732f88e8532ee3378b022a1838fbc4e8c
Juan Jose Fajardo Rodriguez <99266769+juanFajardoR@users.noreply.github.com>
2025-04-11 04:48:59 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -2,8 +2,8 @@ require('@testing-library/jest-dom');
 
 global.matchMedia = global.matchMedia || function(query) {
   return {
-    matches: query === '(prefers-color-scheme: dark)', // Aquí puedes ajustarlo según tu lógica
-    addEventListener: jest.fn(), // Simulamos la función addEventListener
-    removeEventListener: jest.fn(), // Simulamos la función removeEventListener
+    matches: query === '(prefers-color-scheme: dark)',
+    addEventListener: jest.fn(), 
+    removeEventListener: jest.fn(),
   };
 };
